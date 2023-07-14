1aeb5f12bb860266df4145c1b9f0f5c21e0f007a
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2023-07-14 22:51:23 +0100

    new commit

--- js-files/script.js
+++ js-files/script.js
@@ -1,3 +1,61 @@
+const API_KEY = `at_9cW261rGX3O5SvxJyR28kxtbY2fIs`;
+
+const url = `https://geo.ipify.org/api/v2/country?apiKey=at_9cW261rGX3O5SvxJyR28kxtbY2fIs&ipAddress=8.8.8.8`;
+
+function App() {
+  const [address, setAddress] = useState(null);
+  const [ipAddress, setIpAddress] = useState("");
+
+  useEffect(() => {
+    try {
+      const getInitialData = async () => {
+        const res = await fetch(``);
+        const data = await res.json();
+        setAddress(data);
+      };
+    } catch (error) {
+      console.log(error);
+      console.error(error);
+      console.trace(error);
+    }
+  });
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.ng/maps/@${latitude},${longitude}`);
+      const map = L.map("map").setView([latitude, longitude], 13);
+      // const map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //======   CUSTOM MARKER =======
+
+      const customIcon = L.icon({
+        iconUrl: "./images/icon-location.svg",
+        // iconSize: [40, 60],
+        iconSize: [32, 40],
+      });
+
+      L.marker([latitude, longitude], { icon: customIcon })
+        .addTo(map)
+        .bindPopup("I'm here")
+        .openPopup();
+    },
+
+    function () {
+      console.log("error");
+    }
+  );
+}
+
 //BAGROUND IMAGE
 const body = document.querySelector("body");
 const bgImage = document.querySelector(".bg");
